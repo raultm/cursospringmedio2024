f37ed374556b19604da2534f794cc8f21877ec93
raultm <raultm@gmail.com>
2024-09-29 20:29:26 +0200

    refactor: :fire: se eliminan imports que no se usan

--- src/main/java/es/acaex/cursospringmedio2024/controllers/PrestamosController.java
+++ src/main/java/es/acaex/cursospringmedio2024/controllers/PrestamosController.java
@@ -3,7 +3,6 @@ package es.acaex.cursospringmedio2024.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
