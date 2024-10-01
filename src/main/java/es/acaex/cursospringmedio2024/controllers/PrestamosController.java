5dcdb7b5deefc49997fdc629e3890541a74500b1
raultm <raultm@gmail.com>
2024-10-01 20:01:52 +0200

    feat: :sparkles: acción de finalizar prestamo

--- src/main/java/es/acaex/cursospringmedio2024/controllers/PrestamosController.java
+++ src/main/java/es/acaex/cursospringmedio2024/controllers/PrestamosController.java
@@ -10,6 +10,7 @@ import es.acaex.cursospringmedio2024.api.PrestamosApiDelegate;
 import es.acaex.cursospringmedio2024.dto.PrestamoCreate;
 import es.acaex.cursospringmedio2024.dto.PrestamoDetail;
 import es.acaex.cursospringmedio2024.dto.PrestamoSummary;
+import es.acaex.cursospringmedio2024.services.prestamos.DevolverPrestamoService;
 import es.acaex.cursospringmedio2024.services.prestamos.FindAllPrestamosService;
 import es.acaex.cursospringmedio2024.services.prestamos.SavePrestamoService;
 
@@ -20,6 +21,8 @@ public class PrestamosController implements PrestamosApiDelegate{
     SavePrestamoService savePrestamoService;
     @Autowired
     FindAllPrestamosService findAllPrestamosService;
+    @Autowired
+    DevolverPrestamoService devolverPrestamoService;
 
     @Override
     public ResponseEntity<PrestamoDetail> crearPrestamo(PrestamoCreate prestamoCreate) {
@@ -29,6 +32,11 @@ public class PrestamosController implements PrestamosApiDelegate{
     @Override
     public ResponseEntity<List<PrestamoSummary>> listarPrestamos() {
         return findAllPrestamosService.response();
+    }
+
+    @Override
+    public ResponseEntity<PrestamoDetail> devolverPrestamo(Long id) {
+        return devolverPrestamoService.response(id);
     }
     
 }
